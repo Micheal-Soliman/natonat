2a8cbfee3eb27b91ecb57bcebda6733c48b837bc
Micheal Soliman <michealsoliman2@gmail.com>
2026-05-26 15:57:21 +0300

    edits 123

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    qualities: [45, 55, 60, 70, 75],
     minimumCacheTTL: 60,
     unoptimized: true,
     remotePatterns: [
