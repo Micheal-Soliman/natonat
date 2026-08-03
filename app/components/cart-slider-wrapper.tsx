7b9eae15e38fc03fd022015ebd3036eb2a0d9ecb
Micheal Soliman <michealsoliman2@gmail.com>
2026-08-03 14:30:32 +0300

    Defer cart drawer bundle

--- app/components/cart-slider-wrapper.tsx
+++ app/components/cart-slider-wrapper.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { CartSlider } from "./cart-slider";
+import dynamic from "next/dynamic";
+
+const CartSlider = dynamic(
+  () => import("./cart-slider").then((mod) => mod.CartSlider),
+  {
+    ssr: false,
+    loading: () => null,
+  },
+);
 
 export function CartSliderWrapper() {
   return <CartSlider />;
